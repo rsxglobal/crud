27fb173c157cf5c59d8c2d765e8908edc7da360b
David Thibodeaux <rsxglobal@gmail.com>
2017-06-19 17:16:08 -0700

    look mom, no hands

---
+++ CRUDApp.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CRUDApp.Web.MvcApplication" Language="C#" %>
---
+++ CRUDApp.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CRUDApp.Web.Startup))]
+namespace CRUDApp.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
